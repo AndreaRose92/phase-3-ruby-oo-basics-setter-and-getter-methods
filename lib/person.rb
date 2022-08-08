55c6a53a5948c605a3b1b26213441f4f72d8c237
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-08 12:57:32 -0700

    commit tree 839694b743359dacf0e7465ddb6bf7c2c1185722

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
 
+    def job=(job)
+        @job = job
+    end
+    def job
+        @job
+    end
 end
+
+person1 = Person.new
+
+person1.name = "Andrea"
+person1.job = "Coder"
