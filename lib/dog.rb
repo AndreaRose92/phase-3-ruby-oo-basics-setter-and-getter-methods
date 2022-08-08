22199bfd325f53474d07eb1cd2f38e5b6ce869ff
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-08 12:57:44 -0700

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
 
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+    def breed
+        @breed
+    end
 end
+
+
+dog1 = Dog.new
+
+dog1.name = "Austin"
+dog1.breed = "Cat"
